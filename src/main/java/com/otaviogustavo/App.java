fc8f36e4811f3a4b3f307b030724b76113294d17
tavobatista <otavio.batista@bringit.com.br>
2026-05-06 19:13:25 -0300

    Ajustado tamanho da janela

--- src/main/java/com/otaviogustavo/App.java
+++ src/main/java/com/otaviogustavo/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("home"), 640, 480);
+        scene = new Scene(loadFXML("home"), 800, 600);
         stage.setTitle("Baroness Player");
         stage.setScene(scene);
         stage.show();
