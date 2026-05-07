df7d7bfb419af628e67b5dca09d7bd2a7a0531fe
tavobatista <otavio.batista@bringit.com.br>
2026-05-07 17:29:44 -0300

    Refatora estrutura principal da interface

--- src/main/java/com/otaviogustavo/App.java
+++ src/main/java/com/otaviogustavo/App.java
@@ -4,9 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * JavaFX App
@@ -17,18 +19,34 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("home"), 800, 600);
+        scene = new Scene(loadFXML("main"), 1000, 600);
+        loadStyle("main");
         stage.setTitle("Baroness Player");
+        stage.getIcons().add(
+                new Image(getClass().getResourceAsStream("/com/otaviogustavo/images/icons/baroness.png")));
         stage.setScene(scene);
         stage.show();
     }
 
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
+        loadStyle(fxml);
+    }
+
+    private static void loadStyle(String fxml) {
+        URL styleSheet = App.class.getResource("css/" + fxml + ".css");
+
+        if (styleSheet != null) {
+            scene.getStylesheets().add(styleSheet.toExternalForm());
+        }
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("views/" + fxml + ".fxml"));
+        URL fxmlLocation = App.class.getResource("views/" + fxml + ".fxml");
+        if (fxmlLocation == null) {
+            throw new IOException("FXML file not found: views/" + fxml + ".fxml");
+        }
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
         return fxmlLoader.load();
     }
 
